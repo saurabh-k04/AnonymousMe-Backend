49419b8d8e0af3346983d600e316f44b7177248f
saurabh-k04 <68682170+saurabh-k04@users.noreply.github.com>
2025-02-21 18:33:09 +0530

    Update AnonymousMeApplication.java

--- src/main/java/com/hungergames/AnonymousMe/AnonymousMeApplication.java
+++ src/main/java/com/hungergames/AnonymousMe/AnonymousMeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnonymousMeApplication extends SpringBootServletInitializer {
+public class AnonymousMeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnonymousMeApplication.class, args);
