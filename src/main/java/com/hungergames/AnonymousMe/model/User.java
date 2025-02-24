3a47f90065f5eb70b6d336dde72485db029c961d
Saurabh <saurabhkhandelwal548@gmail.com>
2025-02-24 17:14:53 +0530

    Version changes

--- src/main/java/com/hungergames/AnonymousMe/model/User.java
+++ src/main/java/com/hungergames/AnonymousMe/model/User.java
@@ -3,14 +3,14 @@ package com.hungergames.AnonymousMe.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
