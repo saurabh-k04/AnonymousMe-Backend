cb79d45f345d8fcf4c599330d28fcf605eb2838d
saurabh-k04 <68682170+saurabh-k04@users.noreply.github.com>
2025-02-28 20:08:30 +0530

    Update JwtSecurityConfig.java

--- src/main/java/com/anonymous/chat/jwt/JwtSecurityConfig.java
+++ src/main/java/com/anonymous/chat/jwt/JwtSecurityConfig.java
@@ -75,7 +75,7 @@ public class JwtSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));  // ✅ Allow frontend
+        configuration.setAllowedOrigins(List.of("https://anonymousmechat-backend.onrender.com"));  // ✅ Allow frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
